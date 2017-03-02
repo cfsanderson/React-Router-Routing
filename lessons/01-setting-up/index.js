7ac18b9207e6019c17dfd432663176a912d84524
Caleb Frost Sanderson <calebsanderson@gmail.com>
2017-03-01 21:36:59 -0500

    up to session 07

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -4,18 +4,20 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
-import FirstRepo from './modules/FirstRepo'
-import SecondRepo from './modules/SecondRepo'
+// import FirstRepo from './modules/FirstRepo'
+// import SecondRepo from './modules/SecondRepo'
+import Repo from './modules/Repo'
 
 render((
   <Router history={hashHistory}>
     <Route path='/' component={App}>
-      <Route path='/repos' component={Repos}>
-        <Route path='/repos/first-repo' component={FirstRepo}/>
-        <Route path='/repos/second-repo' component={SecondRepo}/>
-      </Route>
+      <Route path='/repos' component={Repos}/>
+      <Route path='/repos/:userName/:repoName' component={Repo}/>
       <Route path='/about' component={About}/>
     </Route>
     {/* putting any of the above routes outside of the App route just gives it a unique path/page. Boxes, inside of boxes, inside of boxes, inside... */}
   </Router>
 ), document.getElementById('app'))
+
+{/* <Route path='/repos/first-repo' component={FirstRepo}/> */}
+{/* <Route path='/repos/second-repo' component={SecondRepo}/> */}
--- lessons/01-setting-up/modules/Repos.js
+++ lessons/01-setting-up/modules/Repos.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-class Repos extends Component {
-
+export default React.createClass({
   render() {
     return (
       <div className='inner-comp'>
-        <h2>Here is the Repo Component</h2>
-        <ul role='nav'>
-          <li><Link  to='/repos/first-repo' activeClassName='active'>First Repo</Link></li>
-          <li><Link  to='/repos/second-repo' activeClassName='active'>Second Repo</Link></li>
+        <h2>Repos</h2>
+        <ul>
+          <li><Link to='/repos/reactjs/react-router'>React Router</Link></li>
+          <li><Link to='/repos/facebook/react'>React</Link></li>
         </ul>
-        {this.props.children}
+        {/* <div>
+          <h2>Here is the Repo Component</h2>
+          <ul role='nav'>
+            <li><Link  to='/repos/first-repo' activeClassName='active'>First Repo</Link></li>
+            <li><Link  to='/repos/second-repo' activeClassName='active'>Second Repo</Link></li>
+          </ul>
+        </div>
+        {this.props.children} */}
       </div>
     )
   }
-}
-
-export default Repos
+})
