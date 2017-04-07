839d06be12ba1ea3f83521fe28e21431d5975c1a
Caleb Sanderson <caleb@Caleb.fios-router.home>
2017-04-06 22:00:04 -0400

    updated some styling and got video working

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -2,25 +2,36 @@ import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import NavLink from './NavLink'
 import Home from './Home'
-// import videoURL from '../images/runners.mp4'
-// import posterURL from '../images/runners-frame1.jpg'
+import videoURL from '../images/runners.mp4'
+import posterURL from '../images/runners-frame1.jpg'
 
 export default React.createClass({
   render() {
     return (
       <div>
         <header className='main-navbar'>
-          {/* <video autoPlay loop muted poster={posterURL} id='video-bg'>
+          <video autoPlay loop muted poster={posterURL} id='video-bg'>
             <source src={videoURL} type='video/mp4' />
-          </video> */}
-          <ul role='nav'>
-            <li><h1><NavLink to='/' onlyActiveOnIndex={true}>Page Template</NavLink></h1></li>
-            <li><NavLink to='/about'>About</NavLink></li>
-            <li><NavLink to='/repos'>Repos</NavLink></li>
-          </ul>
+          </video>
+          <div id="overlay">
+            <h2>overlay</h2>
+            <ul role='nav'>
+              <li><h1><NavLink to='/' onlyActiveOnIndex={true}>Page Template</NavLink></h1></li>
+              <li><NavLink to='/about'>About</NavLink></li>
+              <li><NavLink to='/repos'>Repos</NavLink></li>
+            </ul>
+          </div>
         </header>
+        <ul className="socials">
+          <li><i class="fa fa-twitter-square" aria-hidden="true"></i>tweet</li>
+          <li><i class="fa fa-facebook-square" aria-hidden="true"></i>like</li>
+          <li><i class="fa fa-linkedin-square" aria-hidden="true"></i>link</li>
+          <li><i class="fa fa-google-plus-square" aria-hidden="true"></i>+1</li>
+        </ul>
         {this.props.children}
       </div>
     )
   }
 })
+
+// translucent overlay - http://codepen.io/icutpeople/pen/whueK?editors=1100
--- lessons/01-setting-up/webpack.config.js
+++ lessons/01-setting-up/webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
+      { test: /\.(png|jpe?g|gif|svg|mp4)$/, loader: 'file' }
     ]
   }
 }
